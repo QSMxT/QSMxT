6347e953f02a72f1a0a88522fe881cac48f52ba8
Steffen Bollmann <steffen.bollmann@cai.uq.edu.au>
2018-07-17 16:09:50 +1000

    started but switching to nipype for scalability

--- run_qsm_processing.sh
+++ run_qsm_processing.sh
@@ -1,2 +1,29 @@
-singularity shell shub://CAIsr/qsm
+#!/bin/bash
+set -e 
+#fail early
 
+
+subject=sub-S008LCBL
+
+for echoM in `ls $subject/anat/*gre_M*.nii.gz`; do
+        echo $echoM  
+        bet $echoM ${echoM%.nii.gz}_bet -R -f 0.6 -m
+done
+
+fail
+
+
+for echoNbr in {0..5}; do
+        singularity \
+        exec \
+        --bind $PWD:/data \
+        CAIsr-qsm-v1.2.3-latest.simg \
+        tgv_qsm \
+        -p /data/${subject}_gre6phase_split_000${echoNbr}.nii \
+        -m /data/${subject}_gre6magni_split_000${echoNbr}_bet_mask.nii \
+        -f 2.89 \
+        -e 0 \
+        -t ${echoTime[5]} \
+        -s \
+        -o tgvqsm
+done
