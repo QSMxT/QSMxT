7d57bf2c92e01d75eb74a88eb20b0a9a339ff68e
Steffen <stebo85@web.de>
2018-07-23 15:52:46 +1000

    build my own json reader

--- play.py
+++ play.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from nipype.interfaces.io import BIDSDataGrabber
 
 import nipype_interface_tgv_qsm as tgv
 # import nipype_interface_fsl_tgv as tgv
@@ -15,7 +16,22 @@ import nipype_interface_tgv_qsm as tgv
 # print(maths.cmdline)
 # maths.run()
 
-tgv.QSMappingInterface.help()
-qsm = tgv.QSMappingInterface(file_mask='test_mask.nii', file_phase='test_phase.nii', TE=0.004, b0=7, num_threads=9)
-print(qsm.cmdline)
-qsm.run()
+# tgv.QSMappingInterface.help()
+# qsm = tgv.QSMappingInterface(file_mask='test_mask.nii', file_phase='test_phase.nii', TE=0.004, b0=7, num_threads=9)
+# print(qsm.cmdline)
+# qsm.run()
+
+
+import pprint
+import os
+from nipype.interfaces.io import JSONFileGrabber, JSONFileSink
+
+jsonSource = JSONFileGrabber()
+jsonSource.help()
+jsonSource.inputs.in_file = os.path.join('test.json')
+jsonSource.inputs.defaults = {'EchoTime': 0.0001}
+res = jsonSource.run()
+pprint.pprint(res.outputs.get())
+
+# res.outputs.get('EchoTime')
+
--- run_nipype_qsm.py
+++ run_nipype_qsm.py
@@ -2,8 +2,8 @@
 from os.path import join as opj
 import os
 from nipype.interfaces.fsl import (BET, ImageMaths)
-from nipype.interfaces.utility import IdentityInterface
-from nipype.interfaces.io import SelectFiles, DataSink
+from nipype.interfaces.utility import IdentityInterface, Function
+from nipype.interfaces.io import SelectFiles, DataSink, JSONFileGrabber, JSONFileSink
 from nipype.pipeline.engine import Workflow, Node
 from nipype import MapNode
 import nipype_interface_tgv_qsm as tgv
@@ -11,22 +11,22 @@ import nipype_interface_tgv_qsm as tgv
 os.environ["FSLOUTPUTTYPE"] = "NIFTI_GZ"
 
 experiment_dir = '/QRISdata/Q0538/17042_detection_of_concussion/interim'
-output_dir = '/QRISdata/Q0538/17042_detection_of_concussion/processed'
+output_dir = '/QRISdata/Q0538/17042_detection_of_concussion/derivatives'
 working_dir = '/gpfs1/scratch/30days/uqsbollm/17042_detection_of_concussion'
 
 subject_list = ['sub-S008LCBL']
+
 
 # Infosource - a function free node to iterate over the list of subject names
 infosource = Node(IdentityInterface(fields=['subject_id']), name="infosource")
 infosource.iterables = [('subject_id', subject_list)]
 
-# SelectFiles - to grab the data (alternative to DataGrabber)
-templates = {'mag': '{subject_id}/anat/*gre_M_echo_1.nii.gz',
-             'phs': '{subject_id}/anat/*gre_P_echo_1.nii.gz'}
-selectfiles = Node(SelectFiles(templates, base_directory=experiment_dir), name='selectfiles')
 
-# Datasink - creates output folder for important outputs
-datasink = Node(DataSink(base_directory=experiment_dir, container=output_dir), name='datasink')
+# SelectFiles - to grab the data (alternative to DataGrabber)
+templates = {'mag': '{subject_id}/anat/*gre_M_echo_[1,2].nii.gz',
+             'phs': '{subject_id}/anat/*gre_P_echo_[1,2].nii.gz',
+             'params': '{subject_id}/anat/*gre_P_echo_[1,2].json'}
+selectfiles = Node(SelectFiles(templates, base_directory=experiment_dir), name='selectfiles')
 
 
 # create Nodes
@@ -36,10 +36,29 @@ bet_n = MapNode(BET(frac=0.4, mask=True, robust=True),
 phs_range_n = MapNode(ImageMaths(op_string='-div 4096 -mul 6.28318530718 -sub 3.14159265359'),
                       name='phs_range_node', iterfield=['in_file'])
 
-# load params for qsm from json files here
 
-qsm_n = MapNode(tgv.QSMappingInterface(iterations=30, TE=0.04, b0=7),
-                name='qsm_node', iterfield=['file_phase', 'file_mask'])
+def read_json(in_file):
+    import os
+    if os.path.exists(in_file):
+        import json
+        with open(in_file, 'rt') as fp:
+            data = json.load(fp)
+            EchoTime = data['EchoTime']
+            MagneticFieldStrength = data['MagneticFieldStrength']
+
+    return EchoTime, MagneticFieldStrength
+
+
+params_n = MapNode(interface=Function(input_names=['in_file'],
+                                      output_names=['EchoTime', 'MagneticFieldStrength'],
+                                      function=read_json),
+                   name='read_json', iterfield=['in_file'])
+
+qsm_n = MapNode(tgv.QSMappingInterface(iterations=1, b0=7),
+                name='qsm_node', iterfield=['file_phase', 'file_mask', 'TE', 'b0'])
+
+datasink = Node(DataSink(base_directory=experiment_dir, container=output_dir),
+                name='datasink')
 
 
 # Connect Nodes in  preprocessing workflow
@@ -48,10 +67,14 @@ preproc.base_dir = opj(experiment_dir, working_dir)
 preproc.connect([(infosource, selectfiles, [('subject_id', 'subject_id')]),
                  (selectfiles, bet_n, [('mag', 'in_file')]),
                  (selectfiles, phs_range_n, [('phs', 'in_file')]),
+                 (selectfiles, params_n, [('params', 'in_file')]),
+                 (params_n, qsm_n, [('EchoTime', 'TE')]),
+                 (params_n, qsm_n, [('MagneticFieldStrength', 'b0')]),
                  (bet_n, qsm_n, [('mask_file', 'file_mask')]),
                  (phs_range_n, qsm_n, [('out_file', 'file_phase')]),
-                 (qsm_n, datasink, [('out_qsm', 'qsm_final')]),
+                 (qsm_n, datasink, [('out_qsm', 'qsm')]),
                  ])
 
-# run
-preproc.run('MultiProc', plugin_args={'n_procs': 9})
+# run as MultiProc
+preproc.write_graph(graph2use='flat', format='png', simple_form=False)
+preproc.run('MultiProc', plugin_args={'n_procs': 1})
