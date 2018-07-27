a68a7d1c9df779318410488ac8af323fce760b2f
Steffen <stebo85@web.de>
2018-07-27 12:16:11 +1000

    added a setup description

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+export SINGULARITY_BINDPATH="/data"
+# Container in /data/lfs2/software/singularity/tgvqsm_amd_20180727
+export PATH=$PATH:/data/lfs2/software/singularity/tgvqsm_amd_20180727
+# Container in /data/lfs2/software/singularity/fsl_5p0p11_20180712
+export PATH=$PATH:/data/lfs2/software/singularity/fsl_5p0p11_20180712
+
+#setup a miniconda python environemnt e.g.
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
+
+#install nipype
+conda install --channel conda-forge nipype
+
