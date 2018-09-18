fa93ab11fe60df81efdc57167fcdf7fdc9bca79e
thomshaw92 <thomasbasilshaw@gmail.com>
2018-09-18 21:12:59 +1000

    Revert "initial commit of qsm28 scripts" wrong repo
    This reverts commit 0245e9cbe33c831d2fcc974af92ae67b0762a6e7.

--- 1_preprocessing/pp_submission_script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for subjName in `cat /30days/$USER/subjnames.csv` ; do 
-	qsub -v SUBJNAME=$subjName ~/scripts/QSM28/1_preprocessing/pp_pbs_script.pbs
-done
--- 1_preprocessing/pp_submission_script_ses-12.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for subjName in `cat /30days/$USER/subjnames_ses-12_redo.csv` ; do 
-	qsub -v SUBJNAME=$subjName ~/scripts/OPTIMEX/1_preprocessing/pp_pbs_script_ses-12.pbs
-done
