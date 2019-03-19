cf7d4645c8041aaedfda0a300fccd69eab6846e8
Steffen Bollmann <steffen.bollmann@cai.uq.edu.au>
2019-03-19 00:37:02 +0000

    Update run_1_dicomConversionToBids.sh, run_2_nipype_qsm.py, setup.sh files Deleted run_conversion.sh, run_gitFetchAndReset.sh, run_nipype_qsm.py files

---
+++ run_1_dicomConversionToBids.sh
@@ -0,0 +1,19 @@
+
+
+for file in `ls -1 ../raw/*.tar  | xargs -n 1 basename`; do
+   filename=${file%.tar}
+   echo $filename
+   heudiconv -d '../raw/{subject}.tar' -s $filename -f heuristic.py -c dcm2niix -b -o .
+done
+
+#ALTERNATIVE FOR DICOMs not in a TAR file:
+
+basePath=
+
+#folder=mysubject
+
+echo $basePath
+for folder in `ls $basePath | xargs -n 1 basename`; do
+   echo $folder
+   heudiconv -d $basePath/{subject}/MONOPOLAR*/*.IMA -s $folder -f heuristic.py -c dcm2niix -b -o .
+done
--- run_gitFetchAndReset.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-git fetch origin
-git reset --hard origin/master
