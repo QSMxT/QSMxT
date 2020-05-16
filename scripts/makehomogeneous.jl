9c5781c2c0daa2673639d3f8c254a30ba38802fb
Ashley Stewart <ashley.stewart@uq.edu.au>
2020-05-16 15:16:51 +1000

    make output/work dir if not exist; romeo erosions = 2

--- scripts/makehomogeneous.jl
+++ scripts/makehomogeneous.jl
@@ -1,5 +1,12 @@
 #!/usr/bin/env julia
 using MriResearchTools
-img = readmag(ARGS[1]);
-corrected = makehomogeneous(Float32.(img); σ=[20, 20, 10]);
-savenii(corrected, splitdir(ARGS[2])[2], splitdir(ARGS[2])[1], header(img));
+
+in_path = ARGS[1];
+out_path = ARGS[2];
+out_folder = splitdir(out_path)[1]
+out_file = splitext(splitdir(out_path)[2])[1]
+out_ext = splitext(splitdir(out_path)[2])[2]
+
+mag = readmag(in_path);
+corrected = makehomogeneous(Float32.(mag); σ=[20, 20, 10])
+savenii(corrected, out_file * out_ext, out_folder, header(mag))
--- scripts/romeo_mask.jl
+++ scripts/romeo_mask.jl
@@ -2,9 +2,10 @@
 using MriResearchTools
 
 phase_dir = ARGS[1]
-TEs = [parse(Float64, x) for x in split(ARGS[2], ',')]
+
+# TEs*100 because romeo works in ms rather than s
+TEs = [parse(Float64, x)*100 for x in split(ARGS[2], ',')]
 weights_threshold = parse(Int, ARGS[3])
-#TEs = [5.84,10.63,15.42,20.21,25]
 out_dir = ARGS[4]
 
 phase_nii = readphase(phase_dir)
