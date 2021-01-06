42cd2dc095ea3a752d88dda92832221011004792
Ashley Stewart <ashley.stewart@uq.edu.au>
2021-01-06 18:29:15 +1000

    added dicomSort; added dicomToBids.py; updated bidsmap version

---
+++ run_0_dicomSort.py
@@ -0,0 +1,94 @@
+#!/bin/python
+
+# Adapted from Alex Weston
+# Digital Innovation Lab, Mayo Clinic
+# https://gist.github.com/alex-weston-13/4dae048b423f1b4cb9828734a4ec8b83
+
+import argparse
+import os
+import pydicom # pydicom is using the gdcm package for decompression
+
+def clean_text(string):
+    # clean and standardize text descriptions, which makes searching files easier
+    forbidden_symbols = ["*", ".", ",", "\"", "\\", "/", "|", "[", "]", ":", ";", " "]
+    for symbol in forbidden_symbols:
+        string = string.replace(symbol, "_") # replace everything with an underscore
+    return string.lower()  
+
+def dicomsort(src, dst):
+    os.makedirs(dst, exist_ok=True)
+
+    print('reading file list...')
+    unsortedList = []
+    for root, dirs, files in os.walk(src):
+        for file in files: 
+            if ".IMA" in file:# exclude non-dicoms, good for messy folders
+                unsortedList.append(os.path.join(root, file))
+
+    print('%s files found.' % len(unsortedList))
+        
+    for dicom_loc in unsortedList:
+        # read the file
+        ds = pydicom.read_file(dicom_loc, force=True)
+    
+        # get patient, study, and series information
+        patientID = clean_text(ds.get("PatientID", "NA"))
+        studyDate = clean_text(ds.get("StudyDate", "NA"))
+        studyDescription = clean_text(ds.get("StudyDescription", "NA"))
+        seriesDescription = clean_text(ds.get("SeriesDescription", "NA"))
+    
+        # generate new, standardized file name
+        modality = ds.get("Modality","NA")
+        studyInstanceUID = ds.get("StudyInstanceUID","NA")
+        seriesInstanceUID = ds.get("SeriesInstanceUID","NA")
+        instanceNumber = str(ds.get("InstanceNumber","0"))
+        fileName = modality + "." + seriesInstanceUID + "." + instanceNumber + ".IMA"
+        
+        # uncompress files (using the gdcm package)
+        try:
+            ds.decompress()
+        except:
+            print('an instance in file %s - %s - %s - %s" could not be decompressed. exiting.' % (patientID, studyDate, studyDescription, seriesDescription ))
+    
+        # save files to a 3-tier nested folder structure
+        patientID_date = f"sub-{patientID}_{studyDate}"
+
+        if not os.path.exists(os.path.join(dst, patientID_date)):
+            os.makedirs(os.path.join(dst, patientID_date))
+        
+        if not os.path.exists(os.path.join(dst, patientID_date, studyDescription)):
+            os.makedirs(os.path.join(dst, patientID_date, studyDescription))
+        
+        if not os.path.exists(os.path.join(dst, patientID_date, studyDescription, seriesDescription)):
+            os.makedirs(os.path.join(dst, patientID_date, studyDescription, seriesDescription))
+            print('Saving out file: %s - %s - %s - %s.' % (patientID, studyDate, studyDescription, seriesDescription ))
+        
+        ds.save_as(os.path.join(dst, patientID_date, studyDescription, seriesDescription, fileName))
+
+        if os.path.exists(os.path.join(dst, patientID_date, studyDescription, seriesDescription, fileName)):
+            os.remove(dicom_loc)
+
+    print('done.')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="QSMxT DICOM to BIDS converter",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    parser.add_argument(
+        'src',
+        help='folder containing DICOM files'
+    )
+
+    parser.add_argument(
+        'dst',
+        default=None,
+        const=None,
+        nargs='?',
+        help='output folder to contain sorted DICOMs'
+    )
+
+    args = parser.parse_args()
+    dicomsort(args.src, args.dst if args.dst is not None else args.src)
+    
