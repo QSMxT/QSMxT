27ca4735a034fea02a7381e6027260efed38cc08
Oracle Public Cloud User <opc@gpu.subnet.vcn.oraclevcn.com>
2021-05-13 07:39:43 +0000

    try github container

--- run_test.sh
+++ run_test.sh
@@ -12,9 +12,9 @@ unzip osfstorage/GRE_2subj_1mm_TE20ms/sub1/GR_P_6_QSM_p2_1mmIso_TE20.zip -d dico
 unzip osfstorage/GRE_2subj_1mm_TE20ms/sub2/GR_M_5_QSM_p2_1mmIso_TE20.zip -d dicoms
 unzip osfstorage/GRE_2subj_1mm_TE20ms/sub2/GR_P_6_QSM_p2_1mmIso_TE20.zip -d dicoms
 
-docker run -v /tmp:/tmp vnmd/qsmxt_1.0.0:20210305 python3 /opt/QSMxT/run_0_dicomSort.py /tmp/dicoms /tmp/00_dicom
-docker run -v /tmp:/tmp vnmd/qsmxt_1.0.0:20210305 python3 /opt/QSMxT/run_1_dicomToBids.py /tmp/00_dicom /tmp/01_bids
-docker run -v /tmp:/tmp vnmd/qsmxt_1.0.0:20210305 python3 /opt/QSMxT/run_2_qsm.py /tmp/01_bids /tmp/02_qsm_output
+docker run -v /tmp:/tmp $container python3 /opt/QSMxT/run_0_dicomSort.py /tmp/dicoms /tmp/00_dicom
+docker run -v /tmp:/tmp $container python3 /opt/QSMxT/run_1_dicomToBids.py /tmp/00_dicom /tmp/01_bids
+docker run -v /tmp:/tmp $container python3 /opt/QSMxT/run_2_qsm.py /tmp/01_bids /tmp/02_qsm_output
 
 # don't test right now because of missing data:
 # python3 /opt/QSMxT/run_3_segment.py 01_bids 03_segmentation
@@ -22,7 +22,7 @@ docker run -v /tmp:/tmp vnmd/qsmxt_1.0.0:20210305 python3 /opt/QSMxT/run_2_qsm.p
 # python3 /opt/QSMxT/run_6_analysis --segmentations my_segmentation.nii --qsm_files 05_qsm_template/qsm_transformed/*/*.nii --out_dir 07_analysis
 
 # don't test right now because it takes too long:
-# docker run -it -v /tmp:/tmp vnmd/qsmxt_1.0.0:20210305 python3 /opt/QSMxT/run_4_magnitudeTemplate.py /tmp/01_bids /tmp/04_magnitude_template
+# docker run -it -v /tmp:/tmp $container python3 /opt/QSMxT/run_4_magnitudeTemplate.py /tmp/01_bids /tmp/04_magnitude_template
 
 cd -
 
