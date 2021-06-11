8c456a4fdb6835dcbb4ce4742587366828e901c9
Steffen Bollmann <stebo85@users.noreply.github.com>
2021-06-11 20:33:49 +1000

    more debug output for template tests

--- tests/run_test_template.sh
+++ tests/run_test_template.sh
@@ -22,6 +22,25 @@ unzip /tmp/osfstorage/qsm_final.zip -d /tmp/02_qsm_output_precomputed
 echo "[DEBUG] starting run_4_template.py"
 docker run -v /tmp:/tmp $container python3 /tmp/QSMxT/run_4_template.py /tmp/01_bids /tmp/02_qsm_output_precomputed /tmp/04_template
 
+echo "[DEBUG] testing if outputs are there:"
+echo "[DEBUG] ls /tmp:"
+ls /tmp
+echo "[DEBUG] ls /tmp/04_template:"
+ls /tmp/04_template
+echo "[DEBUG] ls /tmp/04_template/workflow_template:"
+ls /tmp/04_template/workflow_template
+echo "[DEBUG] ls /tmp/04_template/workflow_template/datasink:"
+ls /tmp/04_template/workflow_template/datasink
+echo "[DEBUG] ls /tmp/04_template/workflow_template/datasink/out:"
+ls /tmp/04_template/workflow_template/datasink/out
+echo "[DEBUG] ls /tmp/04_template/workflow_template/datasink/out/test:"
+ls /tmp/04_template/workflow_template/datasink/out/test
+echo "[DEBUG] ls /tmp/04_template/workflow_template/datasink/out/test/results:"
+ls /tmp/04_template/workflow_template/datasink/out/test/results
+
+echo "[DEBUG]: /tmp/04_template/workflow_template/datasink/out/test/results/PassiveTemplate/_ReshapeAveragePassiveImageWithShapeUpdate0/AVG_QSMWARP_AVG_QSM.nii.gz"
 [ -f /tmp/04_template/workflow_template/datasink/out/test/results/PassiveTemplate/_ReshapeAveragePassiveImageWithShapeUpdate0/AVG_QSMWARP_AVG_QSM.nii.gz ] && echo "$FILE exist." || exit 1
+echo "[DEBUG]: /tmp/04_template/workflow_template/datasink/out/test/results/PreRegisterAverage/average.nii"
 [ -f /tmp/04_template/workflow_template/datasink/out/test/results/PreRegisterAverage/average.nii ] && echo "$FILE exist." || exit 1
+echo "[DEBUG]: /tmp/04_template/workflow_template/datasink/out/test/results/PrimaryTemplate/iteration02_Reshaped.nii.gz"
 [ -f /tmp/04_template/workflow_template/datasink/out/test/results/PrimaryTemplate/iteration02_Reshaped.nii.gz ] && echo "$FILE exist." || exit 1
