a332cdf8ebd6d073fd98680ec5b2d9db48b7cb7f
Steffen Bollmann <stebo85@users.noreply.github.com>
2021-06-30 08:10:09 +1000

    output files of qsm where delted so template couldn't run. Try to fix that.

--- tests/run_test_transparent_singularity.sh
+++ tests/run_test_transparent_singularity.sh
@@ -7,10 +7,10 @@ wget -O- http://neuro.debian.net/lists/focal.us-nh.full | sudo tee /etc/apt/sour
 # sudo apt-key adv --recv-keys --keyserver hkp://pool.sks-keyservers.net:80 0xA5D32F012649A5A9
 # keyserver sometimes fails :( -> --allow-unauthenticated as a workaround?
 echo "[DEBUG] sudo apt-get update --allow-insecure-repositories"
-sudo apt-get update --allow-insecure-repositories
+sudo apt-get update --allow-insecure-repositories > /dev/null 2>&1
 echo "[DEBUG] sudo apt-get update --allow-unauthenticated"
-sudo apt-get install --allow-unauthenticated singularity-container 
-sudo apt install singularity-container
+sudo apt-get install --allow-unauthenticated singularity-container > /dev/null 2>&1
+sudo apt install singularity-container > /dev/null 2>&1
 
 cp -r . /tmp/QSMxT
 
@@ -31,8 +31,8 @@ source_command=`cat /tmp/QSMxT/README.md | grep "source activate_qsmxt_"`
 echo $source_command
 $source_command
 
-pip install osfclient
-osf -p ru43c clone /tmp
+pip install osfclient > /dev/null 2>&1
+osf -p ru43c clone /tmp > /dev/null 2>&1
 unzip /tmp/osfstorage/GRE_2subj_1mm_TE20ms/sub1/GR_M_5_QSM_p2_1mmIso_TE20.zip -d /tmp/dicoms > /dev/null 2>&1
 unzip /tmp/osfstorage/GRE_2subj_1mm_TE20ms/sub1/GR_P_6_QSM_p2_1mmIso_TE20.zip -d /tmp/dicoms > /dev/null 2>&1
 unzip /tmp/osfstorage/GRE_2subj_1mm_TE20ms/sub2/GR_M_5_QSM_p2_1mmIso_TE20.zip -d /tmp/dicoms > /dev/null 2>&1
@@ -53,7 +53,7 @@ source ~/.bashrc
 
 conda_command=`cat /tmp/QSMxT/README.md | grep "conda install "`
 echo $conda_command
-$conda_command
+$conda_command > /dev/null 2>&1
 
 echo "[DEBUG] starting run_0_dicomSort.py"
 /usr/share/miniconda/bin/python3 /tmp/QSMxT/run_0_dicomSort.py /tmp/dicoms /tmp/00_dicom
@@ -65,8 +65,10 @@ echo "[DEBUG] starting run_2_qsm.py normal"
 /usr/share/miniconda/bin/python3 /tmp/QSMxT/run_2_qsm.py /tmp/01_bids /tmp/02_qsm_output --n_procs 2 --qsm_iterations 2
 [ -f /tmp/02_qsm_output/qsm_final/_run_run-1/sub-170705134431std1312211075243167001_ses-1_acq-qsmPH00_run-1_phase_scaled_qsm-filled_000_average.nii ] && echo "[DEBUG]. Test OK." || exit 1
 [ -f /tmp/02_qsm_output/qsm_final/_run_run-1/sub-170706160506std1312211075243167001_ses-1_acq-qsmPH00_run-1_phase_scaled_qsm-filled_000_average.nii ] && echo "[DEBUG]. Test OK." || exit 1
-sudo rm -rf /tmp/02_qsm_output
+
 
 echo "[DEBUG] starting run_4_template.py"
 /usr/share/miniconda/bin/python3 /tmp/QSMxT/run_4_template.py /tmp/01_bids /tmp/02_qsm_output /tmp/04_template
+echo "[DEBUG]: /tmp/04_template/workflow_template/datasink/out/test/results"
+[ -d /tmp/04_template/workflow_template/datasink/out/test/results/ ] && echo "results exist." || exit 1
 
