4442bf638b3a69608843c75d570f30ffdbd61dd4
Ashley Stewart <ashley.stewart@uq.edu.au>
2021-12-19 10:57:47 +1000

    updated file matching patterns for template building

--- run_4_template.py
+++ run_4_template.py
@@ -136,7 +136,7 @@ if __name__ == "__main__":
 
     parser.add_argument(
         '--magnitude_pattern',
-        default='{subject}/{session}/anat/*qsm*{run}*magnitude*nii*',
+        default='{subject}/{session}/anat/*{run}*magnitude*nii*',
         help='Pattern to match magnitude files within the BIDS directory. ' +
              'The {subject}, {session} and {run} placeholders must be present.'
     )
@@ -186,7 +186,7 @@ if __name__ == "__main__":
     magnitude_pattern = os.path.join(args.bids_dir, args.magnitude_pattern.format(subject=args.subject_pattern, session=args.session_pattern, run='*'))
     qsm_pattern = os.path.join(args.qsm_dir, args.qsm_pattern)
     magnitude_images = sorted(glob.glob(magnitude_pattern))
-    magnitude_images = [x for x in magnitude_images if 'E01' in x or not re.findall('E[0-9]{2}', x)]
+    magnitude_images = [x for x in magnitude_images if 'echo-1' in x or '_T2starw' in x]
     qsm_images = sorted(glob.glob(qsm_pattern))
 
     if len(magnitude_images) != len(qsm_images):
