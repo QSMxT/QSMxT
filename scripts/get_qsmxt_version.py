043b729459a68f66a2672877931037adc85d0357
Neurodesk User <user@neurodesk.github.io>
2022-07-07 12:02:52 +0000

    fix version date when running from another folder

--- scripts/get_qsmxt_version.py
+++ scripts/get_qsmxt_version.py
@@ -3,7 +3,8 @@ from scripts.sys_cmd import sys_cmd
 
 def get_qsmxt_version():
     this_dir = os.path.dirname(os.path.abspath(__file__))
-    version = sys_cmd(f"git --git-dir {os.path.join(this_dir, '..', '.git')} describe --tags", False, False)
-    date = sys_cmd(f"git log -1 --format=%cd", False, False)
+    git_dir = os.path.join(this_dir, '..', '.git')
+    version = sys_cmd(f"git --git-dir {git_dir} describe --tags", False, False)
+    date = sys_cmd(f"git --git-dir {git_dir} log -1 --format=%cd", False, False)
     return f"{version} (commit date: {date})"
 
