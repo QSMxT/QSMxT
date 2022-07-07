431c28bf94366a7f33afa5520ce24e8051e953e3
Ashley Stewart <a.stewart.au@gmail.com>
2022-07-07 12:00:51 +0000

    use calculated absolute threshold (bugfix)

--- interfaces/nipype_interface_threshold.py
+++ interfaces/nipype_interface_threshold.py
@@ -35,7 +35,7 @@ def thresholding(in_files, op_string=None):
     maxpoint = max(range(len(difference)), key=difference.__getitem__)
     threshold = bin[maxpoint]/np.amax(image_histogram)*100
 
-    op_string =  '-thrp {threshold} -bin -ero'.format(threshold=threshold)
+    op_string =  f"-thr {bin[maxpoint]} -bin -ero"
     iter_op_string = [op_string]*len(in_files)
     return iter_op_string
 
