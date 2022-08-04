79c25b4a3030ea50721f16ffe37b3139303f2dd0
Ashley Stewart <a.stewart.au@gmail.com>
2022-08-04 07:22:16 +0000

    fix sys_cmd to include last character

--- scripts/sys_cmd.py
+++ scripts/sys_cmd.py
@@ -6,7 +6,7 @@ def sys_cmd(cmd, print_output=True, print_command=True):
         print(cmd)
     
     result_byte = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE).stdout
-    results     = result_byte.decode('UTF-8')[:-2]
+    results     = result_byte.decode('UTF-8')[:-1]
     
     if print_output:
         print(results, end="")
