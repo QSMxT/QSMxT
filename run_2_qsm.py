1475b44cab94bafab7c4cee0716bedb8d34965c5
Ashley Stewart <a.stewart.au@gmail.com>
2022-09-14 01:04:18 +0000

    initial solution

--- run_2_qsm.py
+++ run_2_qsm.py
@@ -25,6 +25,7 @@ from interfaces import nipype_interface_bet2 as bet2
 from interfaces import nipype_interface_phase_based as experimental_masking
 from interfaces import nipype_interface_json as json
 from interfaces import nipype_interface_addtojson as addtojson
+from interfaces import nipype_interface_axialsampling as sampling
 
 import argparse
 
@@ -189,6 +190,19 @@ def init_run_workflow(subject, session, run):
         (n_getfiles, mn_params, [('params_files', 'in_file')])
     ])
 
+    # resample to axial
+    mn_resample_inputs = MapNode(
+        interface=sampling.AxialSamplingInterface(
+            obliquity_threshold=10
+        ),
+        iterfield=['in_mag', 'in_pha'],
+        name='nibabel_numpy_nilearn_axial-resampling'
+    )
+    wf.connect([
+        (n_getfiles, mn_resample_inputs, [('magnitude_files', 'in_mag')]),
+        (mn_phase_scaled, mn_resample_inputs, [('out_file', 'in_pha')])
+    ])
+
     # run homogeneity filter if necessary
     if inhomogeneity_correction:
         mn_inhomogeneity_correction = MapNode(
@@ -198,7 +212,7 @@ def init_run_workflow(subject, session, run):
             # output : out_file
         )
         wf.connect([
-            (n_getfiles, mn_inhomogeneity_correction, [('magnitude_files', 'in_file')])
+            (mn_resample_inputs, mn_inhomogeneity_correction, [('out_mag', 'in_file')])
         ])
 
     # do phase weights if necessary
@@ -211,8 +225,8 @@ def init_run_workflow(subject, session, run):
         )
         mn_phaseweights.inputs.weight_type = "grad+second+mag"
         wf.connect([
-            (mn_phase_scaled, mn_phaseweights, [('out_file', 'phase')]),
-            (n_getfiles, mn_phaseweights, [('magnitude_files', 'mag')])
+            (mn_resample_inputs, mn_phaseweights, [('out_pha', 'phase')]),
+            (mn_resample_inputs, mn_phaseweights, [('out_mag', 'mag')])
         ])
 
     # do threshold-based masking if necessary
@@ -244,7 +258,7 @@ def init_run_workflow(subject, session, run):
             ])
         elif masking_method == 'magnitude-based' and not inhomogeneity_correction:
             wf.connect([
-                (n_getfiles, n_threshold_masking, [('magnitude_files', 'in_files')])
+                (mn_resample_inputs, n_threshold_masking, [('out_mag', 'in_files')])
             ])
         else:
             wf.connect([
@@ -265,7 +279,7 @@ def init_run_workflow(subject, session, run):
             ])
         else:
             wf.connect([
-                (n_getfiles, mn_bet, [('magnitude_files', 'in_file')])
+                (mn_resample_inputs, mn_bet, [('out_mag', 'in_file')])
             ])
         mn_bet_erode = MapNode(
             interface=erode.ErosionInterface(
@@ -343,7 +357,7 @@ def init_run_workflow(subject, session, run):
         (mn_params, mn_qsm_filled, [('EchoTime', 'TE')]),
         (mn_params, mn_qsm_filled, [('MagneticFieldStrength', 'b0')]),
         (mn_mask, mn_qsm_filled, [('masks_filled', 'mask_file')]),
-        (mn_phase_scaled, mn_qsm_filled, [('out_file', 'phase_file')]),
+        (mn_resample_inputs, mn_qsm_filled, [('out_pha', 'phase_file')]),
     ])
 
     # qsm averaging
@@ -354,8 +368,19 @@ def init_run_workflow(subject, session, run):
         # output : out_file
     )
     wf.connect([
-        (mn_qsm_filled, n_qsm_filled_average, [('out_file', 'in_files')]),
-        (n_qsm_filled_average, n_datasink, [('out_file', 'qsm_singlepass' if args.two_pass else 'qsm_final')]),
+        (mn_qsm_filled, n_qsm_filled_average, [('out_file', 'in_files')])
+    ])
+
+    # resample qsm to original
+    n_resample_qsm = Node(
+        interface=sampling.ResampleLikeInterface(
+            in_like=magnitude_files[0]
+        ),
+        name='nibabel_numpy_nilearn_resample-qsm'
+    )
+    wf.connect([
+        (n_qsm_filled_average, n_resample_qsm, [('out_file', 'in_file')]),
+        (n_resample_qsm, n_datasink, [('out_file', 'qsm_singlepass' if args.two_pass else 'qsm_final')]),
     ])
 
     # === Two-pass QSM reconstruction (not filled) ===
@@ -384,7 +409,7 @@ def init_run_workflow(subject, session, run):
             (mn_params, mn_qsm, [('EchoTime', 'TE')]),
             (mn_params, mn_qsm, [('MagneticFieldStrength', 'b0')]),
             (mn_mask, mn_qsm, [('masks', 'mask_file')]),
-            (mn_phase_scaled, mn_qsm, [('out_file', 'phase_file')])
+            (mn_resample_inputs, mn_qsm, [('out_pha', 'phase_file')])
         ])
 
         # qsm averaging
@@ -420,8 +445,16 @@ def init_run_workflow(subject, session, run):
             (mn_qsm_twopass, n_qsm_twopass_average, [('out_file', 'in_files')])
         ])
 
+        # resample qsm to original
+        n_resample_qsm_twopass = Node(
+            interface=sampling.ResampleLikeInterface(
+                in_like=magnitude_files[0]
+            ),
+            name='nibabel_numpy_nilearn_resample-qsm-twopass'
+        )
         wf.connect([
-            (n_qsm_twopass_average, n_datasink, [('out_file', 'qsm_final')]),
+            (n_qsm_twopass_average, n_resample_qsm_twopass, [('out_file', 'in_file')]),
+            (n_resample_qsm_twopass, n_datasink, [('out_file', 'qsm_final')]),
         ])
 
     return wf
