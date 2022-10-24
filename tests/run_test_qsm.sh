96cf86963b9d4f5b55623cd7527d1db3e2a51fa2
Ashley <a.stewart.au@gmail.com>
2022-10-24 17:08:36 +1000

    Fix test path

--- tests/run_test_qsm.sh
+++ tests/run_test_qsm.sh
@@ -6,5 +6,5 @@ container=`cat /tmp/QSMxT/README.md | grep -m 1 vnmd/qsmxt | cut -d ' ' -f 6`
 echo "[DEBUG] This is the container I extracted from the readme: $container"
 sudo docker pull $container
 
-sudo docker run -v /tmp:/tmp $container pytest /tmp/QSMxT/scripts/run_test_qsm.py
+sudo docker run -v /tmp:/tmp $container pytest /tmp/QSMxT/tests/run_test_qsm.py
 
