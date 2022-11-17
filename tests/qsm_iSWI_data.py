6e91bdac58fb4ce1ad9f5843cbf64163258eefa8
Korbinian Eckstein <korbinian.eckstein@gmail.com>
2022-11-17 08:23:04 +0000

    bugfix for point in path name

--- tests/qsm_iSWI_data.py
+++ tests/qsm_iSWI_data.py
@@ -8,8 +8,7 @@ def run_qsm(data_dir, out_dir, th1=None, th2=None):
     print(f"Running QSM for {out_dir}")
     args = qsm.process_args(qsm.parse_args([
         data_dir,
-        os.path.join(out_dir, "qsm"),
-        "--tgvqsm_iterations", "1" ## TODO remove when everything seems fine!
+        os.path.join(out_dir, "qsm")
     ]))
     run_args = None
     if th1 is not None and th2 is not None:
@@ -25,7 +24,7 @@ if __name__ == "__main__":
     # export PATH=/neurodesktop-storage/qsmxt/scripts/:$PATH
     
     data_dir = "/neurodesktop-storage/qsm_josef_data/bids"
-    out_dir = "results_josef_data"
+    out_dir = "results_iSWI_data"
     
     # Automatic masking
     current_dir = out_dir + "/automatic_masking"
@@ -33,15 +32,15 @@ if __name__ == "__main__":
         run_qsm(data_dir, current_dir)
     
     # One mask threshold 0.1:0.05:0.8
-    for thresh in np.arange(0.15, 0.8, 0.05):
-        current_dir = out_dir + f"/one_thresh_masking_{thresh:.2f}"
+    for thresh in np.arange(0.0, 0.8, 0.1):
+        current_dir = out_dir + f"/one_thresh_masking_{int(100*thresh)}"
         if not os.path.isdir(current_dir + "/qsm/qsm_final"):
             run_qsm(data_dir, current_dir, thresh)
     
     # Two smallMaskTh 0.1:0.1:0.8 x filledMaskTh 0.1:0.1:0.8
-    # for th1 in np.arange(0.1, 0.8, 0.1):
-    #     for th2 in np.arange(0.1, 0.8, 0.1):
-    #         current_dir = out_dir + f"/two_thresh_masking_{th1:.2f}_{th2:.2f}"
-    #         if not os.path.isdir(current_dir + "/test_analysis"):
-    #             run_qsm(data_dir, current_dir, th1, th2)
+    for th1 in np.arange(0.1, 0.8, 0.1):
+        for th2 in np.arange(0.1, 0.8, 0.1):
+            current_dir = out_dir + f"/two_thresh_masking_{int(100*th1)}_{int(100*th2)}"
+            if not os.path.isdir(current_dir + "/test_analysis"):
+                run_qsm(data_dir, current_dir, th1, th2)
     
