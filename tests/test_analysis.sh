32c7fd66637e6fe5a21c259198548364b73094f9
Ashley Stewart <a.stewart.au@gmail.com>
2022-11-22 23:43:42 +0000

    Deleted unnecessary files

--- tests/test_analysis.sh
+++
@@ -1,5 +0,0 @@
-./run_5_analysis.py \
---segmentations /tmp/bids-osf/sub-1/ses-1/extra_data/sub-1_ses-1_run-01_segmentation.nii.gz \
---qsm_files /tmp/qsm/qsm_final/sub-1_ses-1_run-01_echo-01_part-phase_MEGRE_qsm_000_twopass_average.nii \
---output_dir ../test_analysis \
---qsm_ground_truth /tmp/bids-osf/sub-1/ses-1/extra_data/sub-1_ses-1_run-01_chi-interpolated.nii.gz
