2d33e07f7b89393cb0a49571ba1cc0d4840614f5
Ashley Stewart <a.stewart.au@gmail.com>
2022-11-23 06:39:07 +0000

    Add plot output

--- tests/run_test_qsm.py
+++ tests/run_test_qsm.py
@@ -6,10 +6,12 @@ import tempfile
 import glob
 import nibabel as nib
 import shutil
+import numpy as np
+import pandas as pd
+import seaborn as sns
 import run_2_qsm as qsm
 from scripts.sys_cmd import sys_cmd
-from run_5_analysis import load_labels, update_labels, get_stats_ground_truth
-from scripts.qsmxt_functions import get_qsmxt_dir
+from matplotlib import pyplot as plt
 
 run_workflow = True
 
@@ -27,23 +29,102 @@ def bids_dir():
         sys_cmd(f"rm {os.path.join(tmp_dir, 'bids-osf.tar')}")
     return os.path.join(tmp_dir, 'bids-osf')
 
-def print_metrics(bids_path, qsm_path):
+def display_nii(
+    nii_path=None, data=None, dim=0, title=None, slc=None, dpi=96, size=None, out_png=None, final_fig=True, title_fontsize=12,
+    colorbar=False, cbar_label=None, cbar_orientation='vertical', cbar_nbins=None, cbar_fontsize=None, cbar_label_fontsize=8,
+    **imshow_args
+):
+    data = data if data is not None else nib.load(nii_path).get_fdata()
+    slc = slc or int(data.shape[0]/2)
+    if dim == 0: slc_data = data[slc,:,:]
+    if dim == 1: slc_data = data[:,slc,:]
+    if dim == 2: slc_data = data[:,:,slc]
+    if size:
+        plt.figure(figsize=(size[0]/dpi, size[1]/dpi), dpi=dpi)
+    else:
+        plt.figure(dpi=dpi)
+    plt.axis('off')
+    plt.imshow(np.rot90(slc_data), **imshow_args)
+    if colorbar:
+        cbar = plt.colorbar(orientation=cbar_orientation, fraction=0.037, pad=0.04)
+        if cbar_fontsize:
+            cbar.ax.tick_params(labelsize=cbar_fontsize)
+        if cbar_nbins:
+            cbar.ax.locator_params(nbins=cbar_nbins)
+        if cbar_label:
+            if cbar_orientation == 'horizontal':
+                cbar.ax.set_xlabel(cbar_label, fontsize=cbar_label_fontsize)
+            else:
+                cbar.ax.set_ylabel(cbar_label, fontsize=cbar_label_fontsize, rotation=90)
+    if title:
+        plt.title(title, fontsize=title_fontsize)
+    if final_fig:
+        if out_png:
+            plt.savefig(out_png, bbox_inches='tight')
+        else:
+            plt.show()
+
+def print_metrics(name, bids_path, qsm_path):
     qsm_file = glob.glob(os.path.join(qsm_path, "qsm_final", "*qsm*nii*"))[0]
     seg_file = glob.glob(os.path.join(bids_path, "sub-1", "ses-1", "extra_data", "*segmentation*nii*"))[0]
     chi_file = glob.glob(os.path.join(bids_path, "sub-1", "ses-1", "extra_data", "*chi*crop*nii*"))[0]
+    mask_file = glob.glob(os.path.join(bids_path, "sub-1", "ses-1", "extra_data", "*brainmask*nii*"))[0]
 
     qsm = nib.load(qsm_file).get_fdata()
     seg = nib.load(seg_file).get_fdata()
     chi = nib.load(chi_file).get_fdata()
+    mask = nib.load(mask_file).get_fdata()
+    seg *= mask
+    chi *= mask
 
-    labels = {}
-    update_labels(labels, seg)
-    label_stats = get_stats_ground_truth(labels, seg, qsm, chi)
-    
-    for label_name in label_stats.keys():
-        if label_stats[label_name]:
-            voxels, min_v, max_v, median, mean, std, mean_abs_diff, rms_diff = label_stats[label_name]
-            print(f"{label_name}: voxels={voxels}; min={round(min_v, 4)}; max={round(max_v, 4)}; median={round(median, 4)}; mean={round(mean, 4)}; std={round(std, 4)}; mean_abs_diff={round(mean_abs_diff, 2)}; rms_diff={round(rms_diff, 4)}")
+    labels = { 
+        1 : "Caudate",
+        2 : "Globus pallidus",
+        3 : "Putamen",
+        4 : "Red nucleus",
+        5 : "Dentate nucleus",
+        6 : "SN and STN",
+        7 : "Thalamus",
+        8 : "White matter",
+        9 : "Gray matter",
+        10 : "CSF",
+        11 : "Blood",
+        12 : "Fat",
+        13 : "Bone",
+        14 : "Air",
+        15 : "Muscle",
+        16 : "Calcification"
+    }
+
+    columns = ["Label", "RMSE"]
+
+    # whole brain
+    qsm_values = qsm[mask == 1].flatten()
+    chi_values = chi[mask == 1].flatten()
+    rmse_column = np.sqrt(np.square(qsm_values - chi_values)).reshape(-1,1)
+    labels_column = np.full(rmse_column.shape, "Whole brain")
+    new_vals = np.append(labels_column, rmse_column, axis=1)
+    metrics_np = np.array(new_vals)
+
+    # other areas
+    for label_num in labels.keys():
+        qsm_values = qsm[seg == label_num].flatten()
+        chi_values = chi[seg == label_num].flatten()
+        rmse_column = np.sqrt(np.square(qsm_values - chi_values)).reshape(-1,1)
+        labels_column = np.full(rmse_column.shape, labels[label_num])
+        new_vals = np.append(labels_column, rmse_column, axis=1)
+        metrics_np = np.append(metrics_np, new_vals, axis=0)
+
+    metrics = pd.DataFrame(data=metrics_np, columns=columns)
+    metrics['RMSE'] = metrics['RMSE'].astype(float)
+    plt.figure(figsize=(15, 8), dpi=200)
+    ax = sns.boxplot(data=metrics, x="Label", y="RMSE", color="seagreen")
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha="right")
+    plt.tight_layout()
+    plt.savefig(os.path.join(qsm_path, "qsm_final", "metrics.png"))
+    plt.close()
+
+    display_nii(data=qsm, dim=0, cmap='gray', vmin=-0.1, vmax=+0.1, colorbar=True, cbar_label='ppm', cbar_orientation='horizontal', cbar_nbins=3, out_png=os.path.join(qsm_path, "qsm_final", "slice.png"))
 
 
 def workflow(args, init_workflow, run_workflow, run_args, show_metrics=False):
@@ -57,10 +138,10 @@ def workflow(args, init_workflow, run_workflow, run_args, show_metrics=False):
             for key, value in run_args.items():
                 args_dict[key] = value
             wf = qsm.init_workflow(args)
-        shutil.rmtree(os.path.join(args.output_dir, "qsm_final"), ignore_errors=True)
+        #shutil.rmtree(os.path.join(args.output_dir, "qsm_final"), ignore_errors=True)
         wf.run(plugin='MultiProc', plugin_args={'n_procs': args.n_procs})
         if show_metrics:
-            print_metrics(args.bids_dir, args.output_dir)
+            print_metrics(str(args), args.bids_dir, args.output_dir)
 
 @pytest.mark.parametrize("init_workflow, run_workflow, run_args", [
     (True, run_workflow, { 'tgvqsm_iterations' : 1, 'num_echoes' : 2, 'single_pass' : True })
@@ -110,7 +191,7 @@ def test_args_tgvqsm_defaults(bids_dir, init_workflow, run_workflow, run_args):
     workflow(args, init_workflow, run_workflow, run_args)
 
 @pytest.mark.parametrize("init_workflow, run_workflow, run_args", [
-    (True, run_workflow, { 'num_echoes' : 2, 'n_procs' : 1 })
+    (True, False, { 'num_echoes' : 2, 'n_procs' : 1 })
 ])
 def test_args_nextqsm_defaults(bids_dir, init_workflow, run_workflow, run_args):
     args = qsm.process_args(qsm.parse_args([
@@ -134,7 +215,7 @@ def test_args_nextqsm_defaults(bids_dir, init_workflow, run_workflow, run_args):
     workflow(args, init_workflow, run_workflow, run_args)
     
 @pytest.mark.parametrize("init_workflow, run_workflow, run_args", [
-    (True, run_workflow, { 'num_echoes' : 2, 'n_procs' : 1 })
+    (True, False, { 'num_echoes' : 2, 'n_procs' : 1 })
 ])
 def test_args_nextqsm_laplacian(bids_dir, init_workflow, run_workflow, run_args):
     args = qsm.process_args(qsm.parse_args([
@@ -356,18 +437,19 @@ def test_args_numechoes(bids_dir, init_workflow, run_workflow, run_args):
 
 
 @pytest.mark.parametrize("init_workflow, run_workflow, run_args", [
-    (True, run_workflow, None)
+    (True, run_workflow, { 'tgvqsm_iterations' : 1, 'num_echoes' : 2, 'single_pass' : False })
 ])
 def test_metrics(bids_dir, init_workflow, run_workflow, run_args):
     args = qsm.process_args(qsm.parse_args([
         bids_dir,
-        os.path.join(tempfile.gettempdir(), "qsm")
+        os.path.join(tempfile.gettempdir(), "qsm-metrics"),
+        "--masking", "magnitude-based"
     ]))
     
     assert(args.bids_dir == os.path.abspath(bids_dir))
-    assert(args.output_dir == os.path.join(tempfile.gettempdir(), "qsm"))
+    assert(args.output_dir == os.path.join(tempfile.gettempdir(), "qsm-metrics"))
     assert(args.qsm_algorithm == "tgv_qsm")
-    assert(args.masking == "phase-based")
+    assert(args.masking == "magnitude-based")
     assert(args.two_pass == True)
     assert(args.single_pass == False)
     assert(args.inhomogeneity_correction == False)
