c29e90fe9254b2779102ab0d4947324f4ecd71d5
Ashley Stewart <a.stewart.au@gmail.com>
2022-11-25 01:35:26 +0000

    Test now pulls the appropriate branch

--- tests/run_test_qsm.sh
+++ tests/run_test_qsm.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 set -e 
-git clone https://github.com/QSMxT/QSMxT.git /tmp/QSMxT
+
+branch="${GITHUB_REF##*/}"
+echo "[DEBUG] Pulling QSMxT branch ${branch}..."
+git clone -b "${branch}" "https://github.com/QSMxT/QSMxT.git" "/tmp/QSMxT"
 
 container=`cat /tmp/QSMxT/README.md | grep -m 1 vnmd/qsmxt | cut -d ' ' -f 6`
-echo "[DEBUG] This is the container I extracted from the readme: $container"
-sudo docker pull $container
+echo "[DEBUG] Pulling QSMxT container ${container}..."
+sudo docker pull "${container}"
 
-sudo docker run --env PYTHONPATH=/tmp/QSMxT --env DATA_URL=$DATA_URL --env DATA_PASS=$DATA_PASS -v /tmp:/tmp $container pytest /tmp/QSMxT/tests/run_test_qsm.py -s
+echo "[DEBUG] Running QSM pipeline tests..."
+sudo docker run --env PYTHONPATH=/tmp/QSMxT --env DATA_URL="${DATA_URL}" --env DATA_PASS="${DATA_PASS}" -v /tmp:/tmp "${container}" pytest /tmp/QSMxT/tests/run_test_qsm.py -s
 
