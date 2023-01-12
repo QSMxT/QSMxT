Masking fix for threshold percentage

interfaces/nipype_interface_masking.py
@@ -56,7 +56,7 @@ def threshold_masking(in_files, user_threshold=None, threshold_algorithm='gaussi
             threshold = user_threshold
         else: # user-defined percentage threshold
             data_range = np.max(np.array(all_float_data)) - np.min(np.array(all_float_data))
-            threshold = np.min(data_range) + (user_threshold * data_range)
+            threshold = np.min(np.array(all_float_data)) + (user_threshold * data_range)
         return threshold
 
     # do masking