Fix for determining whether scaling is needed

interfaces/nipype_interface_scalephase.py
@@ -9,7 +9,7 @@ def scale_to_pi(in_file):
     import numpy as np
     nii = nib.load(in_file)
     data = nii.get_fdata()
-    if abs(np.round(np.min(data), 2)) == np.round(np.max(data), 2) == 3.14:
+    if (np.round(np.min(data), 2)*-1) == np.round(np.max(data), 2) == 3.14:
         return in_file
     data = np.array(np.interp(data, (data.min(), data.max()), (-pi, +pi)), dtype=data.dtype)
     out_file = os.path.abspath(os.path.split(in_file)[1].replace(".nii", "_scaled.nii"))