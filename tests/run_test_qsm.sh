c691e0a6b98a1475d0e7997bd9e5b9463d751faa
Neurodesk User <user@neurodesk.github.io>
2023-02-24 09:37:45 +0000

    QSM test now modifies PATH in container to correctly invoke scripts

--- tests/run_test_qsm.sh
+++ tests/run_test_qsm.sh
@@ -20,6 +20,15 @@ container=`cat /tmp/QSMxT/README.md | grep -m 1 vnmd/qsmxt | cut -d ' ' -f 6`
 echo "[DEBUG] Pulling QSMxT container ${container}..."
 sudo docker pull "${container}"
 
+container_path=`sudo docker run ${container} python -c "import os; print(os.environ['PATH'])"`
 echo "[DEBUG] Running QSM pipeline tests..."
-sudo docker run --env BRANCH="${BRANCH}" --env PYTHONPATH=/tmp/QSMxT --env DOWNLOAD_URL="${DOWNLOAD_URL}" --env DATA_PASS="${DATA_PASS}" --env UPLOAD_URL="${UPLOAD_URL}" -v /tmp:/tmp ${container} pytest /tmp/QSMxT/tests/run_test_qsm.py -s
+sudo docker run \
+    --env BRANCH="${BRANCH}" \
+    --env PYTHONPATH=/tmp/QSMxT \
+    --env PATH=/tmp/QSMxT:/tmp/QSMxT/scripts:${container_path} \
+    --env DOWNLOAD_URL="${DOWNLOAD_URL}" \
+    --env DATA_PASS="${DATA_PASS}" \
+    --env UPLOAD_URL="${UPLOAD_URL}" \
+    -v /tmp:/tmp ${container} \
+    pytest /tmp/QSMxT/tests/run_test_qsm.py -s
 
