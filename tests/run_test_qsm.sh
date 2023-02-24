205d76bae1cae24be5454c129501af874cf18adc
Neurodesk User <user@neurodesk.github.io>
2023-02-24 09:43:02 +0000

    Test job GHA job summary

--- tests/run_test_qsm.sh
+++ tests/run_test_qsm.sh
@@ -32,3 +32,9 @@ sudo docker run \
     -v /tmp:/tmp ${container} \
     pytest /tmp/QSMxT/tests/run_test_qsm.py -s
 
+echo "This is the lead in sentence for the list" >> $GITHUB_STEP_SUMMARY
+echo "" >> $GITHUB_STEP_SUMMARY # this is a blank line
+echo "- Lets add a bullet point" >> $GITHUB_STEP_SUMMARY
+echo "- Lets add a second bullet point" >> $GITHUB_STEP_SUMMARY
+echo "- How about a third one?" >> $GITHUB_STEP_SUMMARY
+
