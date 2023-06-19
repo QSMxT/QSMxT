fd9ecadae5a0dc4cb3911dbde93ca9a0a72f7dda
Steffen Bollmann <stebo85@users.noreply.github.com>
2023-06-19 14:06:56 +1000

    documented test machine setup

---
+++ tests/testInstanceSetup.sh
@@ -0,0 +1,13 @@
+#On Nectar: ubuntu 22.04 with docker installed
+sudo docker pull vnmd/qsmxt_2.1.0:20230615
+sudo apt update
+sudo apt install unzip
+sudo apt install python3-pip
+sudo pip3 install osfclient
+osf -p ru43c clone /tmp
+unzip /tmp/osfstorage/GRE_2subj_1mm_TE20ms/sub1/GR_M_5_QSM_p2_1mmIso_TE20.zip -d /tmp/dicoms
+unzip /tmp/osfstorage/GRE_2subj_1mm_TE20ms/sub1/GR_P_6_QSM_p2_1mmIso_TE20.zip -d /tmp/dicoms
+unzip /tmp/osfstorage/GRE_2subj_1mm_TE20ms/sub2/GR_M_5_QSM_p2_1mmIso_TE20.zip -d /tmp/dicoms
+unzip /tmp/osfstorage/GRE_2subj_1mm_TE20ms/sub2/GR_P_6_QSM_p2_1mmIso_TE20.zip -d /tmp/dicoms
+
+#Then snapshot image and get id from image catelogue (e.g. 4adcec13-2291-4bff-987f-d87f98d24379)
