9ae215873eda7e44e7e431b7560fbebe3972d4ed
Ashley Stewart <a.stewart.au@gmail.com>
2023-08-18 15:48:25 +1000

    Fix get_qsmxt_dir for linked install

--- qsmxt/scripts/qsmxt_functions.py
+++ qsmxt/scripts/qsmxt_functions.py
@@ -21,7 +21,7 @@ def get_qsmxt_dir():
         return os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(path))), 'qsmxt')
     else:
         # Return the directory up one level from the current file if running from source
-        return os.path.dirname(os.path.dirname(os.path.dirname(path)))
+        return os.path.dirname(os.path.dirname(path))
 
 def get_qsmxt_version():
     return pkg_resources.get_distribution('qsmxt').version
