0a85613e3a7dfa026d7cf8391664f87bdc618c4f
Ashley Stewart <a.stewart.au@gmail.com>
2023-09-18 10:48:54 +1000

    Update transparent-singularity tests

--- qsmxt/tests/test_transparent_singularity.sh
+++ qsmxt/tests/test_transparent_singularity.sh
@@ -9,55 +9,26 @@ sudo apt-get install -y apptainer
 
 cp -r . /tmp/QSMxT
 
-echo "[DEBUG]: testing the transparent singularity command from the README:"
-clone_command=`cat /tmp/QSMxT/README.md | grep https://github.com/NeuroDesk/transparent-singularity`
-echo $clone_command
-$clone_command
+echo "[DEBUG]: Install QSMxT via transparent-singularity"
+/tmp/QSMxT/docs/_includes/transparent_singularity_install.sh
 
-cd_command=`cat /tmp/QSMxT/README.md | grep "cd qsmxt_"`
-echo $cd_command
-$cd_command
+echo "[DEBUG]: Check an arbitrary executable using cat"
+cat qsmxt
 
-run_command=`cat /tmp/QSMxT/README.md | grep "run_transparent_singularity"`
-echo $run_command
-$run_command
-
-source_command=`cat /tmp/QSMxT/README.md | grep "source activate_qsmxt_"`
-echo $source_command
-$source_command
-
-echo "[DEBUG]: check julia executable:"
-cat julia
-
-echo "[DEBUG]: download test data"
+echo "[DEBUG]: Download test data"
 pip install osfclient > /dev/null 2>&1
 osf -p ru43c clone /tmp > /dev/null 2>&1
 tar xf /tmp/osfstorage/dicoms-unsorted.tar -C /tmp 
 
-echo "[DEBUG]: testing the python setup commands from the README:"
-
-wget_command=`cat /tmp/QSMxT/README.md | grep "wget https://repo.anaconda.com/miniconda"`
-echo $wget_command
-$wget_command > /dev/null 2>&1
-
-bash_command=`cat /tmp/QSMxT/README.md | grep "bash Miniconda3"`
-echo $bash_command
-$bash_command
-
-source ~/.bashrc
-export PATH=/tmp/QSMxT:/usr/share/miniconda/bin:$PATH
-export PYTHONPATH=/tmp/QSMxT:$PYTHONPATH
-
-pip_command=`cat /tmp/QSMxT/README.md | grep "pip install "`
-echo $pip_command
-$pip_command
+echo "[DEBUG]: Install miniconda"
+/tmp/QSMxT/docs/_includes/miniconda_install.sh
 
 echo "[DEBUG] dicom-sort /tmp/dicoms-unsorted /tmp/dicoms-sorted"
 dicom-sort /tmp/dicoms-unsorted /tmp/dicoms-sorted
 
-echo "[DEBUG] /usr/share/miniconda/bin/python3 /tmp/QSMxT/dicom_convert.py /tmp/dicoms-sorted /tmp/bids --auto_yes"
+echo "[DEBUG] dicom-convert /tmp/dicoms-sorted /tmp/bids --auto_yes --t2starw_protocol_patterns '*qsm*' --t1w_protocol_patterns '*mp2rage*'"
 dicom-convert /tmp/dicoms-sorted /tmp/bids --auto_yes --t2starw_protocol_patterns '*qsm*' --t1w_protocol_patterns '*mp2rage*'
 
-echo "[DEBUG] /usr/share/miniconda/bin/python3 /tmp/QSMxT/qsmxt.py /tmp/bids /tmp/out --premade fast --do_qsm --do_template --do_segmentation --do_analysis --auto_yes"
+echo "[DEBUG] qsmxt /tmp/bids /tmp/out --premade fast --do_qsm --do_template --do_segmentation --do_analysis --auto_yes --debug"
 qsmxt /tmp/bids /tmp/out --premade fast --do_qsm --do_template --do_segmentation --do_analysis --auto_yes --debug
 
