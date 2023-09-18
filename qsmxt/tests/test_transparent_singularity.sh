7bb849683cc3d6b027744b81d6b0ffda61ffc7b0
Ashley Stewart <a.stewart.au@gmail.com>
2023-09-18 13:42:06 +1000

    Try to source activate_ script from test script

--- qsmxt/tests/test_transparent_singularity.sh
+++ qsmxt/tests/test_transparent_singularity.sh
@@ -12,8 +12,8 @@ cp -r . /tmp/QSMxT
 echo "[DEBUG]: Install QSMxT via transparent-singularity"
 /tmp/QSMxT/docs/_includes/transparent_singularity_install.sh
 
-echo "[DEBUG]: Source .bashrc"
-source ~/.bashrc
+echo "[DEBUG]: cd qsmxt_* && source activate_qsmxt_${SOFTWARE_VERSION}_${BUILD_DATE}.simg.sh && cd ../"
+cd qsmxt_* && source activate_qsmxt_${SOFTWARE_VERSION}_${BUILD_DATE}.simg.sh && cd ../
 
 echo "[DEBUG]: which tgv_qsm"
 which tgv_qsm
