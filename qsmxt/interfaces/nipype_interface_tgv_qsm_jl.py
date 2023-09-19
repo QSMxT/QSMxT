d0d05e907fb0d65352d10a9dffdecc5362137397
Ashley Stewart <a.stewart.au@gmail.com>
2023-09-19 14:59:37 +1000

    Fix --TE argstring in tgv_qsm.jl interface

--- qsmxt/interfaces/nipype_interface_tgv_qsm_jl.py
+++ qsmxt/interfaces/nipype_interface_tgv_qsm_jl.py
@@ -8,7 +8,7 @@ from qsmxt.scripts.qsmxt_functions import extend_fname, get_qsmxt_dir
 class TGVQSMJlInputSpec(BaseInterfaceInputSpec):
     phase = File(mandatory=True, exists=True, argstr="--phase '%s'")
     mask = File(mandatory=True, exists=True, argstr="--mask '%s'")
-    TE = traits.Float(mandatory=True, argstr="--TE %s'")
+    TE = traits.Float(mandatory=True, argstr="--TE %s")
     vsz = traits.String(value="(1,1,1)", argstr="--vsz \"%s\"")
     B0 = traits.Float(default_value=3, argstr="--b0-str %s")
     qsm = File(name_source=['phase'], name_template='%s_tgvqsmjl.nii', argstr="--output %s")
