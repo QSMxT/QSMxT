310dc2bc83bf3a4acb9ea8ac96e91605b4c08da5
Ashley Stewart <a.stewart.au@gmail.com>
2023-10-18 16:33:07 +1000

    Fix miniconda install command using ==

--- docs/_includes/miniconda_install.sh
+++ docs/_includes/miniconda_install.sh
@@ -3,4 +3,4 @@ bash Miniconda3-4.7.12.1-Linux-x86_64.sh -b
 source ~/.bashrc
 conda create -n qsmxt python=3.8
 conda activate qsmxt
-pip install qsmxt=${PROD_PACKAGE_VERSION}
+pip install qsmxt==${PROD_PACKAGE_VERSION}
