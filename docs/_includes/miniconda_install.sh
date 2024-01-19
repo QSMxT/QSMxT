fa9bbb9117693702b0ff07d7a17d4973664555e2
Ashley Stewart <a.stewart.au@gmail.com>
2024-01-19 16:30:02 +1000

    Update miniconda install

--- docs/_includes/miniconda_install.sh
+++ docs/_includes/miniconda_install.sh
@@ -1,6 +1,7 @@
 wget https://repo.anaconda.com/miniconda/Miniconda3-4.7.12.1-Linux-x86_64.sh	
 bash Miniconda3-4.7.12.1-Linux-x86_64.sh -b -p ${PROD_MINICONDA_PATH}
-source miniconda3/etc/profile.d/conda.sh
+source ${PROD_MINICONDA_PATH}/etc/profile.d/conda.sh
+export PATH=${PROD_MINICONDA_PATH}/bin:${PATH}
 conda create -n qsmxt python=3.8
 conda activate qsmxt
 pip install qsmxt==${PROD_PACKAGE_VERSION}
