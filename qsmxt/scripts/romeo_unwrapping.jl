53468633ec15fc7f481534da5ad99c724ea4579b
Ashley Stewart <a.stewart.au@gmail.com>
2024-03-05 09:45:11 +1000

    Update romeo_unwrapping script to new version

--- qsmxt/scripts/romeo_unwrapping.jl
+++ qsmxt/scripts/romeo_unwrapping.jl
@@ -1,3 +1,12 @@
 #!/usr/bin/env julia
-using RomeoApp
-unwrapping_main(ARGS)
+import Pkg
+Pkg.activate(@__DIR__)
+try
+    using ROMEO, MriResearchTools, ArgParse
+catch
+    Pkg.add(["ROMEO", "MriResearchTools", "ArgParse"])
+    using ROMEO, MriResearchTools, ArgParse
+end
+
+@time msg = unwrapping_main(ARGS)
+println(msg)
