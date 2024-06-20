8abc07f107bab3a2b71a14aa645a1ebff61c46f9
leonardkl1 <120401634+leonardkl1@users.noreply.github.com>
2024-06-20 14:02:44 +0200

    separate install from environment setup

--- docs/_includes/miniconda_install.sh
+++ docs/_includes/miniconda_install.sh
@@ -1,7 +1,10 @@
+#Install Miniconda (skip if conda is already installed)
 wget https://repo.anaconda.com/miniconda/Miniconda3-4.7.12.1-Linux-x86_64.sh	
 bash Miniconda3-4.7.12.1-Linux-x86_64.sh -b -p ${PROD_MINICONDA_PATH}
 source ${PROD_MINICONDA_PATH}/etc/profile.d/conda.sh
 export PATH=${PROD_MINICONDA_PATH}/bin:${PATH}
+
+#create conda environment for qsmxt
 conda create -n qsmxt python=3.8
 conda activate qsmxt
 pip install qsmxt==${PROD_PACKAGE_VERSION}
