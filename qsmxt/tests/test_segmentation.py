78d63154f164ebb9ee92dd99bc7ad1398f4b2a98
Ashley Stewart <a.stewart.au@gmail.com>
2024-07-29 09:52:04 +1000

    Fix syntax error

--- qsmxt/tests/test_segmentation.py
+++ qsmxt/tests/test_segmentation.py
@@ -119,8 +119,8 @@ def test_segmentation(bids_dir_public, init_workflow, run_workflow, run_args):
     if not github_step_summary:
         logger.log(LogLevel.WARNING.value, f"GITHUB_STEP_SUMMARY variable not found! Cannot write summary.")
     else:
-        write_to_file(github_step_summary, f"![result]({upload_png(display_nii(nii_path=find_files(os.path.join(args.bids_dir, 'derivatives', 'qsmxt'), "*_Chimap.nii*")[0], title='QSM', colorbar=True, vmin=-0.1, vmax=+0.1, out_png='qsm.png', cmap='gray'))})")        
-        write_to_file(github_step_summary, f"![result]({upload_png(display_nii(nii_path=find_files(os.path.join(args.bids_dir, 'derivatives', 'qsmxt'), "*_dseg.nii*")[0], title='Segmentation', colorbar=True, vmin=0, vmax=+16, out_png='seg.png', cmap='tab10'))})")        
+        write_to_file(github_step_summary, f"![result]({upload_png(display_nii(nii_path=find_files(os.path.join(args.bids_dir, 'derivatives', 'qsmxt'), '*_Chimap.nii*')[0], title='QSM', colorbar=True, vmin=-0.1, vmax=+0.1, out_png='qsm.png', cmap='gray'))})")        
+        write_to_file(github_step_summary, f"![result]({upload_png(display_nii(nii_path=find_files(os.path.join(args.bids_dir, 'derivatives', 'qsmxt'), '*_dseg.nii*')[0], title='Segmentation', colorbar=True, vmin=0, vmax=+16, out_png='seg.png', cmap='tab10'))})")        
 
         csv_file = find_files(os.path.join(args.bids_dir, 'derivatives', 'qsmxt'), '*analysis*.csv')[0]
         write_to_file(github_step_summary, csv_to_markdown(csv_file))
