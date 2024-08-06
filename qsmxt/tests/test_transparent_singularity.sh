f6d32a0ba8e859a3d3e60b0072745120dd1d6888
Ashley Stewart <a.stewart.au@gmail.com>
2024-08-06 15:17:56 +1000

    update transparent singularity test to use different bids dir

--- qsmxt/tests/test_transparent_singularity.sh
+++ qsmxt/tests/test_transparent_singularity.sh
@@ -12,11 +12,11 @@ tar xf osfstorage/dicoms-unsorted.tar -C .
 echo "[DEBUG] dicom-sort dicoms-unsorted dicoms-sorted"
 dicom-sort dicoms-unsorted dicoms-sorted
 
-echo "[DEBUG] dicom-convert dicoms-sorted bids --auto_yes --qsm_protocol_patterns '*qsm*' --t1w_protocol_patterns '*mp2rage*'"
+echo "[DEBUG] dicom-convert dicoms-sorted bids-transparent-singularity --auto_yes --qsm_protocol_patterns '*qsm*' --t1w_protocol_patterns '*mp2rage*'"
 dicom-convert dicoms-sorted bids --auto_yes --qsm_protocol_patterns '*qsm*' --t1w_protocol_patterns '*mp2rage*'
 
 echo "[DEBUG] bids --premade fast --do_qsm --do_template --do_segmentation --do_analysis --auto_yes --debug"
 qsmxt bids --premade fast --do_qsm --do_template --do_segmentation --do_analysis --auto_yes --debug
 
-rm -rf dicoms-unsorted/ dicoms-sorted/ bids/ 
+rm -rf dicoms-unsorted/ dicoms-sorted/ bids-transparent-singularity/ 
 
