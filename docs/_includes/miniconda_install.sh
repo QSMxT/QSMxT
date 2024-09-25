4c7b2674a6ce03139acbb913e9985287a3123ceb
Steffen Bollmann <stebo85@users.noreply.github.com>
2024-09-26 09:10:59 +1000

    move to miniforge instead of miniconda

--- docs/_includes/miniconda_install.sh
+++ docs/_includes/miniconda_install.sh
@@ -1,8 +1,8 @@
-#Install Miniconda (skip if conda is already installed)
-wget https://repo.anaconda.com/miniconda/Miniconda3-4.7.12.1-Linux-x86_64.sh	
-bash Miniconda3-4.7.12.1-Linux-x86_64.sh -b -p ${PROD_MINICONDA_PATH}
-source ${PROD_MINICONDA_PATH}/etc/profile.d/conda.sh
-export PATH=${PROD_MINICONDA_PATH}/bin:${PATH}
+#Install Miniforge in the current directory. Make sure this directory has enough free space. (skip if conda is already installed)
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
+bash Miniforge3-Linux-x86_64.sh -b -p $PWD/miniforge3
+source $PWD/miniforge3/etc/profile.d/conda.sh
+export PATH=$PWD/miniforge3/bin/:${PATH}
 
 #create conda environment for qsmxt
 conda create -n qsmxt python=3.8
