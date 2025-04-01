6ef330c498c6fa99863c33deee4516d71f1a524d
Ashley Stewart <a.stewart.au@gmail.com>
2025-04-01 14:46:49 +1000

    Update transparent singularity test to not use pattern matching

--- qsmxt/tests/test_transparent_singularity.sh
+++ qsmxt/tests/test_transparent_singularity.sh
@@ -12,8 +12,8 @@ tar xf osfstorage/dicoms-unsorted.tar -C .
 echo "[DEBUG] dicom-sort dicoms-unsorted dicoms-sorted"
 dicom-sort dicoms-unsorted dicoms-sorted
 
-echo "[DEBUG] dicom-convert dicoms-sorted bids-transparent-singularity --auto_yes --qsm_protocol_patterns '*qsm*' --t1w_protocol_patterns '*mp2rage*'"
-dicom-convert dicoms-sorted bids-transparent-singularity --auto_yes --qsm_protocol_patterns '*qsm*' --t1w_protocol_patterns '*mp2rage*'
+echo "[DEBUG] dicom-convert dicoms-sorted bids-transparent-singularity --auto_yes
+dicom-convert dicoms-sorted bids-transparent-singularity --auto_yes
 
 echo "[DEBUG] bids-transparent-singularity --premade fast --do_qsm --do_template --do_segmentation --do_analysis --auto_yes --debug"
 qsmxt bids-transparent-singularity --premade fast --do_qsm --do_template --do_segmentation --do_analysis --auto_yes --debug
