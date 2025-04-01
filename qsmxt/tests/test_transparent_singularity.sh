529624f322c60d9619d6805edda8910c47a53e37
Ashley Stewart <a.stewart.au@gmail.com>
2025-04-01 16:04:44 +1000

    Remove dicom-sort from tests, fix synax error

--- qsmxt/tests/test_transparent_singularity.sh
+++ qsmxt/tests/test_transparent_singularity.sh
@@ -9,10 +9,7 @@ pip install osfclient > /dev/null 2>&1
 osf -p ru43c clone . > /dev/null 2>&1
 tar xf osfstorage/dicoms-unsorted.tar -C .
 
-echo "[DEBUG] dicom-sort dicoms-unsorted dicoms-sorted"
-dicom-sort dicoms-unsorted dicoms-sorted
-
-echo "[DEBUG] dicom-convert dicoms-sorted bids-transparent-singularity --auto_yes
+echo "[DEBUG] dicom-convert dicoms-sorted bids-transparent-singularity --auto_yes"
 dicom-convert dicoms-sorted bids-transparent-singularity --auto_yes
 
 echo "[DEBUG] bids-transparent-singularity --premade fast --do_qsm --do_template --do_segmentation --do_analysis --auto_yes --debug"
