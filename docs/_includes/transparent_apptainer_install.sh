ddb6827f470e0a37b77fe21fb070ed437ae6afe1
Ashley Stewart <a.stewart.au@gmail.com>
2025-04-17 10:24:53 +1000

    Fix install script

--- docs/_includes/transparent_apptainer_install.sh
+++ docs/_includes/transparent_apptainer_install.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/astewartau/transparent-apptainer qsmxt_${PROD_CONTAINER_VERSION}_${PROD_CONTAINER_DATE}
 cd qsmxt_${PROD_CONTAINER_VERSION}_${PROD_CONTAINER_DATE}
-./run_transparent_apptainer.sh ${apptainer_opts} --container qsmxt_${PROD_CONTAINER_VERSION}_${PROD_CONTAINER_DATE}.simg
+./run_transparent_apptainer.sh --container qsmxt_${PROD_CONTAINER_VERSION}_${PROD_CONTAINER_DATE}.simg
 source activate_qsmxt_${PROD_CONTAINER_VERSION}_${PROD_CONTAINER_DATE}.simg.sh
