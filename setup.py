18771452fb8f629b491063ea6e7f61ab63ce8334
Ashley Stewart <a.stewart.au@gmail.com>
2025-08-08 11:20:52 +1000

    Update dicompare dependency to v0.1.28

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
         'nextqsm==1.0.4',
         'matplotlib==3.7.5',
         'pandas==2.0.3',
-        'dicompare==0.1.27'
+        'dicompare==0.1.28'
     ],
     extras_require={
         'dev': [
